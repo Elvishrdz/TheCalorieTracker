773e6939eaa24407d7430261482f0e19bb7669d2
elvishrdz <elvishrdz@gmail.com>
2022-12-28 17:50:06 +0100

    updating dependencies
    some dependencies keep their current version due to incompatibilities or issues during the project compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,19 @@ import org.apache.tools.ant.taskdefs.condition.Os
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.3.1" apply false
-    id("com.android.library") version "7.3.1" apply false
-    id("org.jetbrains.kotlin.android") version Kotlin.version apply false
-    id("org.jetbrains.kotlin.jvm") version Kotlin.version apply false
-    id("com.github.ben-manes.versions") version BenManes.version
-    id("org.jlleitschuh.gradle.ktlint") version Ktlint.version
+    id(Android.applicationPlugin) version Android.applicationPluginVersion apply false
+    id(Android.libraryPlugin) version Android.libraryPluginVersion apply false
+    id(Kotlin.androidPlugin) version Kotlin.androidPluginVersion apply false
+    id(Kotlin.jvmPlugin) version Kotlin.jvmPluginVersion apply false
+    id(BenManes.dependencyUpdatesPlugin) version BenManes.dependencyUpdatesPluginVersion
+    id(Ktlint.ktlintPlugin) version Ktlint.ktlintPluginVersion
 }
 
 buildscript {
     dependencies {
-        classpath(Build.androidBuildTools)
-        classpath(Build.hiltAndroidGradlePlugin)
-        classpath(Build.kotlinGradlePlugin)
+        classpath(Android.toolsBuildPlugin)
+        classpath(DaggerHilt.hiltAndroidGradlePlugin)
+        classpath(Kotlin.gradlePlugin)
     }
 }
 
