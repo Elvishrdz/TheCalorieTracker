44c12e7a60d6bf3b57ef972ba159452d6a4849b6
elvishrdz <elvishrdz@gmail.com>
2022-12-28 10:08:49 +0100

    adding AppModule.kt
    provides preferences implementation using android shared preferences

---
+++ app/src/main/java/com/eahm/thecalorietracker/di/AppModule.kt
@@ -0,0 +1,40 @@
+package com.eahm.thecalorietracker.di
+
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+import com.eahm.core.data.preferences.DefaultPreferences
+import com.eahm.core.domain.preferences.Preferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(
+        app: Application,
+    ): SharedPreferences {
+        return app.getSharedPreferences(
+            /* name = */
+            "shared_pref",
+            /* mode = */
+            MODE_PRIVATE,
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferences(
+        sharedPreferences: SharedPreferences,
+    ): Preferences {
+        return DefaultPreferences(
+            sharedPref = sharedPreferences,
+        )
+    }
+}
